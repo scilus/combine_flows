fe2338b8ad535d63c369ead58a53b1c3a7a6c9db
Maxime Descoteaux <descotea@beluga4.int.ets1.calculquebec.ca>
2020-11-19 15:35:29 -0500

    supporting noddi and fw

---
+++ add_fw_metrics_tractometry.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+usage() { echo "$(basename $0) [-n freewater_flow/results] [-o output]" 1>&2; exit 1; }
+
+while getopts "n:o:" args; do
+    case "${args}" in
+        n) n=${OPTARG};;
+        o) o=${OPTARG};;
+        *) usage;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${n}" ] || [ -z "${o}" ]; then
+    usage
+fi
+
+echo "freewater_flow results folder: ${n}"
+echo "Output folder: ${o}"
+
+echo "Building tree..."
+cd ${n}
+for i in *[!Compute_Kernel]; 
+do
+    echo $i
+    mkdir -p $o/$i/metrics/
+
+    # freewater_flow metrics
+    ln -s $n/$i/Compute_FreeWater/*FW.nii.gz $o/$i/metrics/freewater.nii.gz
+    ln -s $t/$i/FW_Corrected_Metrics/*fa.nii.gz $o/$i/metrics/FAt.nii.gz
+    ln -s $t/$i/FW_Corrected_Metrics/*ad.nii.gz $o/$i/metrics/ADt.nii.gz
+    ln -s $t/$i/FW_Corrected/*rd.nii.gz $o/$i/metrics/RDt.nii.gz
+
+done
+echo "Done"
---
+++ add_noddi_metrics_tractometry.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+usage() { echo "$(basename $0) [-n noddi/results] [-o output]" 1>&2; exit 1; }
+
+while getopts "n:o:" args; do
+    case "${args}" in
+        n) n=${OPTARG};;
+        o) o=${OPTARG};;
+        *) usage;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${n}" ] || [ -z "${o}" ]; then
+    usage
+fi
+
+echo "noddi_flow results folder: ${n}"
+echo "Output folder: ${o}"
+
+echo "Building tree..."
+cd ${n}
+for i in *[!Compute_Kernel]; 
+do
+    echo $i
+    mkdir -p $o/$i/metrics/
+
+    # noddi_flow metrics
+    ln -s $n/$i/Compute_NODDI/*OD.nii.gz $o/$i/metrics/noddi_od.nii.gz
+    ln -s $n/$i/Compute_NODDI/*ISOVF.nii.gz $o/$i/metrics/noddi_isovf.nii.gz
+    ln -s $n/$i/Compute_NODDI/*ICVF.nii.gz $o/$i/metrics/noddi_icvf.nii.gz
+done
+echo "Done"

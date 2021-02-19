23272af5315e05e1020538ef84b9c37d05246eb6
Guillaume Theaud <guillaume.theaud@gmail.com>
2021-02-19 16:57:17 -0500

    New script for freewater

---
+++ tree_for_fw_flow.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+usage() { echo "$(basename $0) [-t tractoflow/results] [-o output]" 1>&2; exit 1; }
+
+while getopts "t:o:" args; do
+    case "${args}" in
+        t) t=${OPTARG};;
+        o) o=${OPTARG};;
+        *) usage;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${t}" ] || [ -z "${o}" ]; then
+    usage
+fi
+
+echo "tractoflow folder: ${t}"
+echo "Output folder: ${o}"
+
+echo "Building tree for the following folders:"
+cd $t
+for i in *[!{FRF}];
+do
+   echo $i
+   mkdir -p $o/$i
+
+   ln -s ${t}/${i}/Resample_DWI/*dwi_resampled.nii.gz ${o}/${i}/dwi.nii.gz
+   ln -s ${t}/${i}/Resample_B0/*b0_mask_resampled.nii.gz ${o}/${i}/brain_mask.nii.gz
+   ln -s ${t}/${i}/Eddy*/*bval_eddy ${o}/${i}/bval
+   ln -s ${t}/${i}/Eddy*/*dwi_eddy_corrected.bvec ${o}/${i}/bvec
+done
+
+rm -rf ${o}/Readme*
+rm -rf ${o}/Read_BIDS
